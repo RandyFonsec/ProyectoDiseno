d5cb0aa9ffb4994b60e60ac79abb0bca2bf40007
RandyFonsec <49202815+RandyFonsec@users.noreply.github.com>
2022-05-07 20:56:59 -0600

    Lista vista de todos los funcionarios

--- routes/RoutesAdmin.js
+++ routes/RoutesAdmin.js
@@ -17,6 +17,26 @@ routerAdmin.get('/gestionPlacas', (req, res) => {
     res.send('Placas');
 })
 
+routerAdmin.get('/edicionFuncionarios', (req, res) => {
+    let { id } = req.query;
+    var lista = [];
+    if (id) {
+        res.render('edicionFuncionarios.ejs', { data: lista });
+    } else {
+        res.render('edicionFuncionarios.ejs', { data: lista });
+    }
+
+
+
+});
+
+routerAdmin.get('/edicionFuncionarios/:id', (req, res) => {
+    let { id } = req.params;
+    console.log(id);
+    console.log("HERE");
+    res.send(id);
+});
+
 
 //export this router to use in our index.js
 module.exports = routerAdmin;
